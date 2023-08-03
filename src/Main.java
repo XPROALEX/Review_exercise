bc5e9b5b4f668b53acff9f4c0860590214e2200f
Alex <alex97av@gmail.com>
2023-08-03 19:57:45 +0200

    fix

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,14 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Student sAlessandroV = new Student(1, "Alessandro", "Venuti", LocalDate.of(1997, 07, 14));
-        Student sMarioR = new Student(2, "Mario", "Rossi", LocalDate.of(1997, 1, 26), Arrays.asList(18, 21, 16, 29));
+        Student sMarioR = new Student(2, "Mario", "Rossi", LocalDate.of(1997, 1, 26), Arrays.asList(23, 21, 26, 29));
         Student sGiorgioB = new Student(3, "Giorgio", "Bianchi", LocalDate.of(1999, 5, 17), Arrays.asList(17, 16, 19, 20));
         Student sLuciaF = new Student(4, "Lucia", "Franchi", LocalDate.of(2000, 2, 13));
 
         Professor pAntonioC = new Professor(1, "Antonio", "Cavallaro", "Arte");
         Professor pMartinaB = new Professor(2, "Martina", "Bontempo");
 
-        pMartinaB.assingGrade(sAlessandroV, 17);
+        pMartinaB.assingGrade(sAlessandroV, 22);
         pAntonioC.assingGrade(sLuciaF, 16);
 
         Student[] studentArray = {sAlessandroV, sMarioR, sGiorgioB, sLuciaF};

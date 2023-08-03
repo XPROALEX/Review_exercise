6361a51148560a2571cbf9655754bf74b593d75b
Alex <alex97av@gmail.com>
2023-08-03 12:39:41 +0200

    add professor class con getter e setter

---
+++ src/Professor.java
@@ -0,0 +1,52 @@
+public class Professor {
+    private int professorId;
+    private String name;
+    private String surname;
+    private String course;
+
+    public Professor(int professorId, String name, String surname, String course) {
+        this.professorId = professorId;
+        this.name = name;
+        this.surname = surname;
+        this.course = course;
+    }
+
+    public Professor(int professorId, String name, String surname) {
+        this.professorId = professorId;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public int getProfessorId() {
+        return professorId;
+    }
+
+    public void setProfessorId(int professorId) {
+        this.professorId = professorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+}

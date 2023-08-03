2ae84c3a6e084fd4a30a12232bce93fccf182955
Alex <alex97av@gmail.com>
2023-08-03 12:55:12 +0200

    add main class

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void  main(String[]args){
+        Student alessandro=new Student(14,"Alessandro","Venuti", LocalDate.of(1997,07,14));
+    }
+}
